5c540b4b720adc5a1f9f1c25d7ef1e3eddcd6257
akramhar <alex.83@bluewin.ch>
2019-06-12 06:38:05 -0400

    work in progress

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import MicroscopeMetadataTool from "4dn-microscopy-metadata-tool";
+
+class MicroscopeMetadataToolOmero extends React.PureComponent {
+	//onLoadMicroscopes={props.onLoadMicroscopes}
+	//onSaveMicroscope = { props.onWorkingDirectorySave }
+	render() {
+		<MicroscopeMetadataTool
+			width={props.dims.width}
+			height={props.dims.height}
+			onLoadSchema={props.onLoadSchema}
+			imagesPath={imagesPath}
+		/>;
+	}
+}
