364ca1b08337deebbbe76956be28fdf2adb1a2f2
William Moore <w.moore@dundee.ac.uk>
2019-06-12 11:54:53 +0100

    basic django app setup

---
+++ omero_microscopyMetadataTool/apps.py
@@ -0,0 +1,6 @@
+
+from django.apps import AppConfig
+
+class MMAppConfig(AppConfig):
+    name = "omero_microscopyMetadataTool"
+    label = "micro-meta-app"
--- omero_microscopyMetadataTool/urls.py
+++ omero_microscopyMetadataTool/urls.py
@@ -1,15 +1,12 @@
-import django
-from omeroweb.webgateway import views as webgateway_views
+
 from . import views
-if django.VERSION < (1, 6):
-    from django.conf.urls.defaults import url, patterns
-else:
-    from django.conf.urls import url, patterns
+from django.conf.urls import url, patterns
 
-
+print "MM urls.py"
 urlpatterns = patterns(
 
     'django.views.generic.simple',
-    # index 'home page' of the figure app
-    url(r'^$', views.index, name='figure_index'),
+
+    url(r'^$', views.index, name='microscopyMetadataTool_index'),
+
 )
