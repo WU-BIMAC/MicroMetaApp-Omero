5843dec69323c46ef722dc167cf26f5a5042ccb1
akramhar <alex.83@bluewin.ch>
2020-11-09 01:31:16 -0500

    update css + version

--- deploy_build.sh
+++ deploy_build.sh
@@ -4,7 +4,7 @@
 echo "Deploying built resources to web app directory..."
 mkdir -p omero_microscopyMetadataTool/static/microscopyMetadataTool/css
 cp public/fontStyle.css omero_microscopyMetadataTool/static/microscopyMetadataTool/css/
-cp node_modules/4dn-microscopy-metadata-tool/public/style.css omero_microscopyMetadataTool/static/microscopyMetadataTool/css/
+cp node_modules/4dn-microscopy-metadata-tool/public/assets/css/style-new.css omero_microscopyMetadataTool/static/microscopyMetadataTool/css/
 
 mkdir -p omero_microscopyMetadataTool/static/microscopyMetadataTool/js
 cp dist/MicroscopyMetadataToolOmero.dev.js omero_microscopyMetadataTool/static/microscopyMetadataTool/js/
