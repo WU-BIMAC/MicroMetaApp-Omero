9b2de718c22e133eeb70978d43e17c50df545a43
akramhar <alex.83@bluewin.ch>
2021-04-17 03:28:22 -0400

    updated deploy script

--- deploy_build.sh
+++ deploy_build.sh
@@ -6,8 +6,11 @@ mkdir -p omero_microMetaApp/static/microMetaAppOmero/css
 cp public/fontStyle.css omero_microMetaApp/static/microMetaAppOmero/css/
 cp node_modules/micro-meta-app-react/public/assets/css/style-new.css omero_microMetaApp/static/microMetaAppOmero/css/
 
+mkdir -p omero_microMetaApp/templates/microMetaAppOmero
+cp public/index.html omero_microMetaApp/templates/microMetaAppOmero/
+
 mkdir -p omero_microMetaApp/static/microMetaAppOmero/js
-cp dist/MicroMetaAppOmero.dev.js omero_microMetaApp/static/microMetaAppOmero/js/
+cp build/MicroMetaAppOmero.dev.js omero_microMetaApp/static/microMetaAppOmero/js/
 
 mkdir -p omero_microMetaApp/static/microMetaAppOmero/images
 mkdir -p omero_microMetaApp/static/microMetaAppOmero/images/png
@@ -15,3 +18,4 @@ mkdir -p omero_microMetaApp/static/microMetaAppOmero/images/svg
 cp public/assets/svg/* omero_microMetaApp/static/microMetaAppOmero/images/svg
 cp public/assets/png/* omero_microMetaApp/static/microMetaAppOmero/images/png
 
+tar -xcvf ./dist/omero_microMetaApp-1.0.0-b1-0.tar ./omero_microMetaApp
