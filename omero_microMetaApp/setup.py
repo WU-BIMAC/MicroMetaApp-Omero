0efc64fe03f0ccec413930a78038e5d4fe2be510
akramhar <alex.83@bluewin.ch>
2021-06-11 10:20:34 -0400

    added setup.py

---
+++ omero_microMetaApp/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='omero-microMetaApp',
+    version='1.2.2-b1-1',
+    description="OMERO.web plugin for Instrument and Image Acquisition Setting Metadata",
+    packages=find_packages(),
+    keywords=['omero', 'microscope', 'metadata', 'micro-meta-app']
+)
